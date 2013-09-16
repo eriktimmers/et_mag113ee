5192dc20e3f15f6e14c8709ff0dbd03a7e7c0377
erik timmers <erik@eriktimmers.eu>
2013-09-16 15:15:39 +0200

    [ET] Tweede

---
+++ app/code/community/kega/Bannergrid/Block/Bannergrid.php
@@ -0,0 +1,11 @@
+<?php
+
+class Kega_Bannergrid_Block_Bannergrid extends Mage_Core_Block_Abstract implements Mage_Widget_Block_Interface
+{
+	protected function _toHtml()
+	{
+		$html = '...';
+		return $html;
+	}
+
+}
